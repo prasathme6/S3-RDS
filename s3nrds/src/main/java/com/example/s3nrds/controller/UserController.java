33c11af79d432f92ce2d5dd9d64fdd1d3ba3f4f0
unknown <prasath20046@gmail.com>
2026-09-01 16:33:12 +0530

    first commit:

--- s3nrds/src/main/java/com/example/s3nrds/controller/UserController.java
+++ s3nrds/src/main/java/com/example/s3nrds/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/users")
 public class UserController {
 
